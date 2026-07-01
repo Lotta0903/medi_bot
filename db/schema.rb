f3ed7e1d19273167bb259ad07b896362805d34bb
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <ps.ringdal@outlook.com>
2026-07-01 08:13:53 +0100

    add content column to medication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_01_053707) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_01_071316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_01_053707) do
   end
 
   create_table "medications", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", null: false
     t.string "dosage"
     t.string "name"
