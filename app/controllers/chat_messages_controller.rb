941a0f2000dbdb2f0d14df36b45cf30aa1af944e
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <ps.ringdal@outlook.com>
2026-07-01 10:50:06 +0100

    add ai assistant

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -1,4 +1,8 @@
 class ChatMessagesController < ApplicationController
+  SYSTEM_PROMT = "You are a helpful medical assistant specializing in medicine and geriatric care.
+   I am a eldery patient who seeking help about my medication
+   Help me understand my medication in simple, non-technical language. Answer in simple plain text"
+
   def create
     @chat = current_user.chats.find(params[:chat_id])
     @medication = @chat.medication
@@ -7,6 +11,10 @@ class ChatMessagesController < ApplicationController
     @message.chat = @chat
     @message.role = "user"
     if @message.save
+      ruby_llm_chat = RubyLLM.chat(model: 'gpt-4.1-nano')
+      ruby_llm_chat.with_instructions(SYSTEM_PROMT)
+      response = ruby_llm_chat.ask(@message.content)
+      ChatMessage.create(role: "assistant", content: response.content, chat: @chat)
       redirect_to chat_path(@chat)
     else
       render "chats/show", status: 422
